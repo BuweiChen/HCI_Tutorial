40b6fa09dc9a2b1e958b3a975c05c3228a953506
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-04-09 23:32:57 -0400

    Initial commit

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
-let host = "cpsc484-01.yale.internal:8888";
-$(document).ready(() => frames.statusbar());
+var host = "cpsc484-01.stdusr.yale.internal:8888";
 
+$(document).ready(function () {
+  frames.start();
+  twod.start();
+});
+
+var frames = {
+  socket: null,
+
+  start: function () {
+    var url = "ws://" + host + "/frames";
+    frames.socket = new WebSocket(url);
+    frames.socket.onmessage = function (event) {
+      frames.show(JSON.parse(event.data));
+    };
+  },
+
+  show: function (frame) {
+    console.log(frame);
+  },
+};
+
+var depth = {
+  socket: null,
+
+  start: function () {
+    var url = "ws://" + host + "/depth";
+    frames.socket = new WebSocket(url);
+    frames.socket.onmessage = function (event) {
+      frames.show(JSON.parse(event.data));
+    };
+  },
+
+  show: function (depth) {
+    $("img.depth").attr("src", "data:image/pnjpegg;base64," + depth.src);
+  },
+};
+
+var twod = {
+  socket: null,
+
+  // create a connection to the camera feed
+  start: function () {
+    var url = "ws://" + host + "/twod";
+    twod.socket = new WebSocket(url);
+
+    // whenever a new frame is received...
+    twod.socket.onmessage = function (event) {
+      // parse and show the raw data
+      twod.show(JSON.parse(event.data));
+    };
+  },
+
+  // show the image by adjusting the source attribute of the HTML img object previously created
+  show: function (twod) {
+    $("img.twod").attr("src", "data:image/pnjpegg;base64," + twod.src);
+  },
+};
